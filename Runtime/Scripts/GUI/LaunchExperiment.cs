ba9ae8a280575e05e92ca079850d7e7118354c29
James Bruska <jamesbthecoder@gmail.com>
2024-03-08 21:58:55 -0300

    Fixed GPLv3 licensing

--- Runtime/Scripts/GUI/LaunchExperiment.cs
+++ Runtime/Scripts/GUI/LaunchExperiment.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿//Copyright (c) 2024 Jefferson University
+//Copyright (c) 2024 Bruska Technologies LLC
+//Copyright (c) 2023 University of Pennsylvania
+
+//This file is part of UnityEPL.
+//UnityEPL is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//UnityEPL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//You should have received a copy of the GNU General Public License along with UnityEPL. If not, see <https://www.gnu.org/licenses/>. 
+
+using System;
 using System.Collections;
 using System.Threading.Tasks;
 using UnityEngine;
