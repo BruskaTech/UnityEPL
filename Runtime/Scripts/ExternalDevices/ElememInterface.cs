48d63e5d77990659d11a95b289d61802d24c64bc
James Bruska <jamesbthecoder@gmail.com>
2024-09-13 10:48:34 -0400

    Remove wrong using that fails the build

--- Runtime/Scripts/ExternalDevices/ElememInterface.cs
+++ Runtime/Scripts/ExternalDevices/ElememInterface.cs
@@ -7,7 +7,6 @@
 //UnityEPL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 //You should have received a copy of the GNU General Public License along with UnityEPL. If not, see <https://www.gnu.org/licenses/>. 
 
-using Codice.Client.Common;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
